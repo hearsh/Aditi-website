fe6fad631d8e0db478f9868dc607d27d3ad55fed
hearsh <hearsh.m@gmail.com>
2020-09-08 22:15:33 -0400

    Removed overlay

--- public/js/app.js
+++ public/js/app.js
@@ -97,5 +97,9 @@ const runStartScripts = async () => {
   checkHeader(window.scrollY);
 }
 
+$(function(){
+  $(".twentytwenty-container[data-orientation!='vertical']").twentytwenty({default_offset_pct: 0.5});
+  $(".twentytwenty-container[data-orientation='vertical']").twentytwenty({default_offset_pct: 0.3, orientation: 'vertical'});
+});
 
 runStartScripts();
