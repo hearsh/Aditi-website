7270e3b97e2f358eccd75a4751c4269e95b48932
hearsh <hearsh.m@gmail.com>
2020-09-12 18:03:23 -0400

    Added js for slider

--- public/js/jquery.twentytwenty.js
+++ public/js/jquery.twentytwenty.js
@@ -83,6 +83,7 @@
           const leftMenu = document.getElementById('left-menu');
           console.log(leftMenu);
           leftMenu.style.width = `${width}px`;
+          leftMenu.style.height = `${offset.ch}`;
         }
       }
 
